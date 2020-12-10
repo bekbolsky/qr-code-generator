7f6f85b23d34f8dd7b3d6cc782d27e01daa3ca8e
Bekbolat Kuralbayev <bkb@live.ru>
2020-12-10 18:24:47 +0600

    Update qr-vcard.py
    Removed sensitive personal data

--- qr-vcard.py
+++ qr-vcard.py
@@ -1,15 +1,15 @@
 from segno import helpers
 
 
-UDU_vCard = helpers.make_vcard(
-    name='Scientific-Educational Journal;"Uly Dala Ustazy"',
-    displayname='"Uly Dala Ustazy" Scientific-Educational Journal',
-    email="dala.ustazy@gmail.com",
-    phone="+7 747 555 9060",
+my_vCard = helpers.make_vcard(
+    name='Kuralbayev;Bekbolat',
+    displayname='"Bekbolat Kuralbayev',
+    email="example@example.com",
+    phone="+7 123 444 5566",
 )
 
-UDU_vCard.save(
-    "UDU_vCard_qr.svg",
+my_vCard.save(
+    "my_vCard_qr.svg",
     scale=6,
     border=0,
     dark="white",
@@ -19,5 +19,3 @@ UDU_vCard.save(
     svgns=False,
     svgclass=False,
 )
-
-print(UDU_vCard.designator)
